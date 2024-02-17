36333712bcf1aa341e0cf67b587028003d3fa179
opteacher <opteacher@hotmail.com>
2024-02-18 02:11:50 +0800

    解码器页面测试中，摄像头工作正常

--- src/views/Decoder.vue
+++ src/views/Decoder.vue
@@ -1,3 +1,29 @@
 <template>
-  Decoder
+  <a-typography-paragraph v-if="!supported">
+    所用浏览器不支持调用摄像头，无法进行解码操作，请更换其他设备尝试！
+  </a-typography-paragraph>
+  <video v-else id="vdoReceiver" class="w-full h-full" autoplay></video>
 </template>
+
+<script setup lang="ts">
+import { waitFor } from '@lib/utils'
+import { onMounted, ref } from 'vue'
+
+const supported = ref<boolean>(false)
+
+onMounted(async () => {
+  supported.value = 'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices
+  const vdoReceiver = (await waitFor('vdoReceiver')) as HTMLVideoElement
+  const pageSize = await waitFor('app').then(ele => ele?.getBoundingClientRect())
+  if (supported.value) {
+    vdoReceiver.srcObject = await navigator.mediaDevices.getUserMedia({
+      video: {
+        width: pageSize?.width,
+        height: pageSize?.height,
+        facingMode: { exact: "environment" },  
+      },
+      audio: false
+    })
+  }
+})
+</script>
